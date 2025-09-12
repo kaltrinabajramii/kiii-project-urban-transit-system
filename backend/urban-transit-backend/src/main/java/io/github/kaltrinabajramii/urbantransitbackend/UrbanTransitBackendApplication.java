d5c11dd771d8dc0be72f68d953fadbce6e093c25
kaltrinabajramii <kaltro.bajrami@icloud.com>
2025-09-12 17:56:56 +0200

    Create backend and frontend

---
+++ backend/urban-transit-backend/src/main/java/io/github/kaltrinabajramii/urbantransitbackend/UrbanTransitBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.kaltrinabajramii.urbantransitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanTransitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( UrbanTransitBackendApplication.class, args );
+    }
+
+}
---
+++ backend/urban-transit-backend/src/test/java/io/github/kaltrinabajramii/urbantransitbackend/UrbanTransitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kaltrinabajramii.urbantransitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanTransitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
